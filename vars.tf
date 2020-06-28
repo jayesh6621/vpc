c9e8fa8d9ce92ebf1e83c3133aef1e6cda75b9a6
jayesh6621 <jayesh6621@gmail.com>
2020-06-28 14:01:26 +0000

    first commit

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+    ap-south-1 = "ami-54d2a63b"
+  }
+}
+
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
